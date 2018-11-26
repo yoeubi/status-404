6058cd7b2004cbfc9926509a5e11391495fe9153
JungwonSeong <yoeubi28@gmail.com>
2018-11-26 20:57:01 +0900

    feat : add modal effect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import styles from './Login.module.scss';
-import classNames from 'classnames';
-import img from './../../img/login.jpg'
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 class Login extends Component {
+    state = {
+        show : false
+    }
+    handleClick = () => {
+        console.log('실행중');
+        
+        this.setState({show: !this.state.show})
+    }
     render() {
+        const show = this.state.show;
         return (
-            <div>
-                
+            <div className={cx('background')}>
+                <div className={cx('modal',{show})}></div>
+                <button onClick={this.handleClick}>클릭</button>
             </div>
         );
     }
