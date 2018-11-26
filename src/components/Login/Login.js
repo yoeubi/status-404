e0d3a09e494f485855228e8a4643dbe740f6579d
JungwonSeong <yoeubi28@gmail.com>
2018-11-26 20:23:52 +0900

    feat : add Font

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import styles from './Login.module.scss';
 import classNames from 'classnames';
+import img from './../../img/login.jpg'
 
 class Login extends Component {
     render() {
         return (
             <div>
-                123123123         
+                
             </div>
         );
     }
