0d6323540f8fa041831ed80727a94caf4fe62b98
hellomac87 <hellomac87@gmail.com>
2018-11-27 00:25:05 +0900

    Feat :: Main Component SVG 이미지 추가 및 적용

--- src/components/Main/MainView.js
+++ src/components/Main/MainView.js
@@ -1,27 +1,117 @@
 import React, { Component } from 'react';
 import styles from './Main.module.scss';
-import classNames from 'classnames';
+import classNames from 'classnames/bind';
+import chevronDown from '../../img/chevron-down.svg';
 
+import hansik from '../../img/01_hansik.svg';
+import bunsik from '../../img/02_bunsik.svg';
+import ilsik from '../../img/03_ilsik.svg';
+import chicken from '../../img/04_chicken.svg';
+import pizza from '../../img/05_pizza.svg';
+import chinese from '../../img/06_chinese.svg';
+import zokbal from '../../img/07_zokbal.svg';
+import yasik from '../../img/08_yasik.svg';
+import zzim from '../../img/09_zzim.svg';
+import dosirak from '../../img/10_dosirak.svg';
+import desert from '../../img/11_desert.svg';
+import fastfood from '../../img/12_fastfood.svg';
+import franchise from '../../img/13_franchise.svg';
+import matzip from '../../img/14_matzip.svg';
+
+const cx = classNames.bind(styles);
 
 class MainView extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            isLogin: true
+            isLogin: true,
+            lists: [
+                {
+                    id:0,
+                    name: '한식',
+                    img: hansik
+                },
+                {
+                    id:1,
+                    name: '분식',
+                    img: bunsik
+                },
+                {
+                    id:2,
+                    name: '돈까스·회·일식',
+                    img: ilsik
+                },
+                {
+                    id:3,
+                    name: '치킨',
+                    img: chicken
+                },
+                {
+                    id:4,
+                    name: '피자',
+                    img: pizza
+                },
+                {
+                    id:5,
+                    name: '중국집',
+                    img: chinese
+                },
+                {
+                    id:6,
+                    name: '족발·보쌈',
+                    img: zokbal
+                },
+                {
+                    id:7,
+                    name: '야식',
+                    img: yasik
+                },
+                {
+                    id:8,
+                    name: '찜·탕',
+                    img: zzim
+                },
+                {
+                    id:9,
+                    name: '도시락',
+                    img: dosirak
+                },
+                {
+                    id:10,
+                    name: '카페·디저트',
+                    img: desert
+                },
+                {
+                    id:11,
+                    name: '패스트푸드',
+                    img: fastfood
+                },
+                {
+                    id:12,
+                    name: '프랜차이즈',
+                    img: franchise
+                },
+                {
+                    id:13,
+                    name: '맛집랭킹',
+                    img: matzip
+                },
+            ]
         }
     }
     render() {
-        const {isLogin} = this.state;
+        const {isLogin, lists} = this.state;
 
         return (
-            <div className={classNames(styles.wrap)}>
+            <div className={cx('wrap')}>
                 
-                <div className={classNames(styles.header)}>
+                <div className={cx('header')}>
                 {
                     isLogin ? (
-                        <div className={classNames(styles.addressInput)}>
-                            <span className="address">상계동 666</span>    
+                            <div className={cx('addressInput')}>
+                            <span className={cx('address')}>상계동 666</span>
+                            <img className={cx('icon')} src={chevronDown} alt="down"/>
                         </div>
                     ):(
                         <p>로그인 및 회원가입 안내 문구</p>
@@ -30,11 +120,30 @@ class MainView extends Component {
                     
                 </div>
 
-                <div className={classNames(styles.body)}>
-                    body
+                <div className={cx('body')}>
+                    <div className={cx('carousel')}>
+                        carousel
+                    </div>
+
+                    <ul className={cx('categories')}>
+                        {
+                            lists.map(c => {
+                                return (
+                                    <li 
+                                        className={cx('item')}
+                                        key={c.name}
+                                    >
+                                        <img src={c.img} alt={c.name}/>
+                                        {c.name}
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                    
                 </div>
                 
-                <div className={classNames(styles.footer)}>
+                <div className={cx('footer')}>
                     footer
                 </div>
             </div>
