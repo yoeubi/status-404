7ca7efaa165c0ebc0b7113f7fc689be7d9d19ae2
JungwonSeong <yoeubi28@gmail.com>
2018-11-27 10:28:46 +0900

    라이브러리 다운을 위한 깃 커밋

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -15,53 +15,53 @@ class Login extends Component {
     }
     render() {
         const show = this.state.show;
-        return (
-            <div className={cx('background')}>
-                <div className={cx('modal',{show})}>
-                    <div className={cx('header')}>
-                        <div>유저 이미지</div>
-                        <div>
-                            <p>고마운 분</p>
-                            <p>deceiver22</p>
-                            <p>0p</p>
-                        </div>
-                    </div>
-                    <div>
-                        <ul className={cx('nav')}>
-                            <li className={cx('item')}>포인트</li>
-                            <li className={cx('item')}>쿠폰함</li>
-                            <li className={cx('item')}>장바구니</li>
-                            <li className={cx('item')}>바로결제내역</li>
-                            <li className={cx('item')}>단골매장</li>
-                            <li className={cx('item')}>리뷰관리</li>
-                        </ul>
-                    </div>
-                    <div className={cx('banner')}>
-                    </div>
-                    <div>
-                        <ul className={cx('menu')}>
-                            <li className={cx('item')}>공지사항</li>
-                            <li className={cx('item')}>이벤트</li>
-                            <li className={cx('item')}>광고문의</li>
-                            <li className={cx('item')}>1:1 문의하기</li>
-                            <li className={cx('item')}>환경설정</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <p>
-                            <span>배달의민족 콜센터</span>
-                            <span>1644-0025</span>
-                            <span>(24시간 운영, 연중무휴)</span>
-                        </p>
-                        <p className={cx('footer')}>
-                            <span>Copyright Status 404 in Fastcampus. All Rights Reserved.</span>
-                            <span>Version 8.9.1</span>
-                        </p>
-                    </div>
+        return <div className={cx("background")}>
+            <div className={cx("modal", { show })}>
+              <div className={cx("header")}>
+                <div>유저 이미지</div>
+                <div>
+                  <p>고마운 분</p>
+                  <p>deceiver22</p>
+                  <p>0p</p>
                 </div>
-                <button onClick={this.handleClick}>클릭</button>
+              </div>
+              <div>
+                <ul className={cx("nav")}>
+                  <li className={cx("item")}>포인트</li>
+                  <li className={cx("item")}>쿠폰함</li>
+                  <li className={cx("item")}>장바구니</li>
+                  <li className={cx("item")}>바로결제내역</li>
+                  <li className={cx("item")}>단골매장</li>
+                  <li className={cx("item")}>리뷰관리</li>
+                </ul>
+              </div>
+              <div className={cx("banner")} />
+              <div>
+                <ul className={cx("menu")}>
+                  <li className={cx("item")}><a href="#">공지사항</a></li>
+                  <li className={cx("item")}><a href="#">이벤트</a></li>
+                  <li className={cx("item")}><a href="#">광고문의</a></li>
+                  <li className={cx("item")}><a href="#">1:1 문의하기</a></li>
+                  <li className={cx("item")}><a href="#">환경설정</a></li>
+                </ul>
+              </div>
+              <div className={cx("footer")}>
+                <p>
+                  <span>배달의민족 콜센터</span>
+                  <span>1644-0025</span>
+                  <span>(24시간 운영, 연중무휴)</span>
+                </p>
+                <p>
+                  <span>
+                    Copyright Status 404 in Fastcampus. All Rights
+                    Reserved.
+                  </span>
+                  <span>Version 8.9.1</span>
+                </p>
+              </div>
             </div>
-        );
+            <button onClick={this.handleClick}>클릭</button>
+          </div>;
     }
 }
 
