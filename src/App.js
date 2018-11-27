2e9e47c32a9e1e3aea843fd2963726b06e992172
hellomac87 <hellomac87@gmail.com>
2018-11-27 15:23:02 +0900

    feat : react router 적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import MainPage from './Pages/MainPage';
 import Login from './components/Login/Login';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <MainPage />
-        <Login />
-      </div>
+      <Router>
+        <Switch>
+          <Route path={'/'} component={MainPage}/>
+          <Login />
+        </Switch>
+      </Router>
     );
   }
 }
