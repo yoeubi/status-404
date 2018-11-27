c3756c8cdf2da694d34fc430c6d01eb75e241a26
Yoon jae Park <yoonjaepark@Yoons-MacBook-Pro.local>
2018-11-27 17:16:21 +0900

    feat: develop AddressSearch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Login from './components/Login/Login';
+import AddressSearch from './components/AddressSearch/AddressSearch'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Login />
+        <AddressSearch />
       </div>
     );
   }
--- src/components/AddressSearch/AddressSearch.js
+++ src/components/AddressSearch/AddressSearch.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react'
+import "./AddressSearch.scss";
 
-export default class AddressSearch extends Component {
+class AddressSearch extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+  
   render() {
     return (
-      <div>
-        
+      <div className="Container">
+        <div className="AddressSearch">
+          <button>X</button>
+          <h1 className="header">지번, 도로명, 건물명을 입력하세요</h1>
+          <input className="input" label="주소검색" type="text" name="address" />
+          <button>돋보기</button>
+          <button>현 위치로 주소 설정</button>
+          <h2>최근 주소</h2>
+        </div>
       </div>
     )
   }
 }
+
+export default AddressSearch
