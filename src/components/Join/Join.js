fec2af6d1e579bd86d7229793e9e5234d5f6ca0e
JungwonSeong <yoeubi28@gmail.com>
2018-11-27 22:04:44 +0900

    feat : change margin in social-login login.scss

---
+++ src/components/Join/Join.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './Join.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
+
+class Join extends Component {
+    render() {
+        return (
+            <div className={cx('join')}>
+                
+            </div>
+        );
+    }
+}
+
+export default Join;
