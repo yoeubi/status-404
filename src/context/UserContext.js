c74af2a566e6d4336604608abde3764180ce8d0b
hellomac87 <hellomac87@gmail.com>
2018-11-28 11:02:39 +0900

    Feat : UserContext 작성

---
+++ src/context/UserContext.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+const {Provider, Consumer: UserConsumer} = React.createContext();
+
+class UserProvider extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user: {
+                id: 0,
+                nickname: '닉네임 이강산',
+                username: '이강산',
+                phone:'000-0000-0000',
+                is_host: true,
+                address: '서울시 노원구 상계동 666',
+                avatar : null
+            }
+        }
+    }
+    render() {
+        return (
+            <Provider value={this.state}>
+                {this.props.children}
+            </Provider>
+        );
+    }
+}
+
+function withUser(WrappedComponent){
+    return function WithUser(props){
+        return (
+            <UserConsumer>
+                {
+                    (value) => <WrappedComponent {...props} {...value}/>
+                }
+            </UserConsumer>
+        )
+    }
+}
+
+export {
+    UserProvider,
+    withUser
+}
