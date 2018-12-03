e3505e024c461ec0087873161034eac1d346e233
hellomac87 <hellomac87@gmail.com>
2018-12-03 12:54:34 +0900

    style : RestaurantDetailView style

--- src/components/StoreDetailView/RestaurantDetailView.js
+++ src/components/StoreDetailView/RestaurantDetailView.js
@@ -3,12 +3,17 @@ import React, { Component } from "react";
 import styles from "./RestaurantDetailView.module.scss";
 import classNames from "classnames/bind";
 
+// SVG ICONS
+import { ReactComponent as Heart } from "../../img/heart.svg"; // 하트 아이콘
+import { ReactComponent as ChevronLeft } from "../../img/chevron-left.svg"; // 뒤로가기 아이콘
+
 const cx = classNames.bind(styles);
 
 class RestaurantDetailView extends Component {
   static defaultProps = {
     id: null,
     name: "배민 상점",
+    rating: 4.5, // 상점 별점
     userId: null,
     is_register: null, // 상점 생성 시간?
     address: "배민구 배민동 000",
@@ -25,15 +30,31 @@ class RestaurantDetailView extends Component {
         params: { id }
       }
     } = this.props;
-    const { name, address, img_profile } = this.props;
+    const { name, address, img_profile, rating } = this.props;
+    const stars = null;
     return (
       <div className={cx("StoreDetail")}>
-        <div className={cx("Header")}>
-          <div className={cx("Prev")}>prev</div>
+        <div className={cx("Header", { Scroll: false })}>
+          <ChevronLeft className={cx("Prev")} />
+          <h1 className={cx("Title")}>{name}</h1>
+          <Heart className={cx("Like")} />
         </div>
 
-        <div>
-          <img src={img_profile} alt={name} />{" "}
+        <div className={cx("Summary")}>
+          <div className={cx("ImgProfile")}>
+            <img style={{ width: "100%" }} src={img_profile} alt={name} />{" "}
+          </div>
+          <div className={cx("BasicInfo")}>
+            <h2>{name}</h2>
+            <div className={cx("Stars")}>
+              <span>{stars}</span>
+              <span className={cx("Rating")}>{rating}</span>
+            </div>
+            <div className={cx("Reviews")}>
+              <span>최근 리뷰 00</span>
+              <span>사장님 최근 댓글 00</span>
+            </div>
+          </div>
         </div>
         <div>상점 아이디 : {id}</div>
         <div>상점 이름 : {name}</div>
