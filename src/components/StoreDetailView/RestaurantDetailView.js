373e240bb0aa64a4af2cfc0c83cf811980548af0
hellomac87 <hellomac87@gmail.com>
2018-12-03 15:59:42 +0900

    feat : react-rating 별점 패키지 적용 & 스크롤 이벤트 적용

--- src/components/StoreDetailView/RestaurantDetailView.js
+++ src/components/StoreDetailView/RestaurantDetailView.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Rating from "react-rating";
 
 import styles from "./RestaurantDetailView.module.scss";
 import classNames from "classnames/bind";
@@ -7,6 +8,7 @@ import classNames from "classnames/bind";
 import { ReactComponent as Heart } from "../../img/heart.svg"; // 하트 아이콘
 import { ReactComponent as ChevronLeft } from "../../img/chevron-left.svg"; // 뒤로가기 아이콘
 import { ReactComponent as Star } from "../../img/star.svg"; // 뒤로가기 아이콘
+import { ReactComponent as ArrowUp } from "../../img/arrow-up.svg"; // 뒤로가기 아이콘
 
 const cx = classNames.bind(styles);
 
@@ -24,7 +26,24 @@ class RestaurantDetailView extends Component {
     img_profile:
       "https://cdn.dominos.co.kr/admin/upload/goods/20180827_ca1sFpdy.jpg"
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      isTop: true
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener("scroll", () => {
+      const isTop = window.scrollY < 100;
+      if (isTop !== this.state.isTop) {
+        this.setState({ isTop });
+      }
+    });
+  }
+
   render() {
+    const { isTop } = this.state;
     const {
       match: {
         // storeId
@@ -32,10 +51,9 @@ class RestaurantDetailView extends Component {
       }
     } = this.props;
     const { name, address, img_profile, rating } = this.props;
-    const stars = null;
     return (
       <div className={cx("StoreDetail")}>
-        <div className={cx("Header", { Scroll: false })}>
+        <div className={cx("Header", { Scroll: !isTop })}>
           <ChevronLeft className={cx("Prev")} />
           <h1 className={cx("Title")}>{name}</h1>
           <Heart className={cx("Like")} />
@@ -48,21 +66,106 @@ class RestaurantDetailView extends Component {
           <div className={cx("BasicInfo")}>
             <h2>{name}</h2>
             <div className={cx("Stars")}>
-              <Star className={cx("Star", "Empty")} />
+              {/* <Star className={cx("Star", "Empty")} />
               <Star className={cx("Star", "Half")} />
-              <Star className={cx("Star")} />
-              <span>{stars}</span>
+              <Star className={cx("Star")} /> */}
+              <Rating
+                // 빈 별
+                emptySymbol={<Star className={cx("Star", "Empty")} />}
+                // 꽉찬별
+                fullSymbol={<Star className={cx("Star")} />}
+                // 분할
+                fractions={2}
+                // 레이팅 정보
+                initialRating={rating}
+                readonly
+              />
               <span className={cx("Rating")}>{rating}</span>
             </div>
             <div className={cx("Reviews")}>
-              <span>최근 리뷰 00</span>
-              <span>사장님 최근 댓글 00</span>
+              <span>최근 리뷰 00</span> | <span>사장님 최근 댓글 00</span>
             </div>
           </div>
+
+          <ul className={cx("OrderInfo")}>
+            <li>
+              <label>결재방법</label><span>바로결재, 만나서 결재</span>
+            </li>
+            <li>
+              <label>최소주문금액</label><span>바로결재, 만나서 결재</span>
+            </li>
+            <li>
+              <label>배달시간</label>
+              <span>45분 소요 예상</span>
+            </li>
+            <li>
+              <label>배달팁</label><span>2,000원</span>
+            </li>
+          </ul>
+
+          <div className={cx("SNS")}>
+            <button>
+              <Heart /> 찜 351
+            </button>
+            <button>
+              <ArrowUp />
+              공유
+            </button>
+          </div>
         </div>
-        <div>상점 아이디 : {id}</div>
-        <div>상점 이름 : {name}</div>
-        <div>상점 주소 : {address}</div>
+
+        <div>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+          <p>test</p>
+        </div>
       </div>
     );
   }
