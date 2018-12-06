6a73173b5dfdd8bf52dfa4c2056d4b6c456b590e
Yoon jae Park <pyoonj0953@gmail.com>
2018-12-06 21:56:55 +0900

    feat: add order button handle functions

--- src/containers/StoreReviewTapContainer.js
+++ src/containers/StoreReviewTapContainer.js
@@ -5,13 +5,35 @@ export default class StoreReviewTapContainer extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      // 리뷰탭에 사진리뷰버튼 활성화 여부
+      activePhotoReviewBtn: false,
+      // 리뷰탭에 리뷰 정렬 버튼 활성화 상태
+      orderBtn: ""
+    };
   }
+
+  // 리뷰탭에 '사진리뷰만' 버튼 활성화 상태 관리 함수
+  handlePhotoReviewBtn = () => {
+    this.setState({
+      activePhotoReviewBtn: true
+    });
+  };
+
+  // 리뷰탭에 리뷰 정렬 순서 버튼 상태 관리 함수
+  handleOrderBtn = btnName => {
+    this.setState({
+      orderBtn: btnName
+    });
+  };
 
   render() {
     return (
       <>
-        <StoreReviewTap />
+        <StoreReviewTap
+          onHandlePhotoReviewBtn={this.handlePhotoReviewBtn}
+          onHandleOrderBtn={this.handleOrderBtn}
+        />
       </>
     );
   }
