95896fbb8f9a4050a37fda9c719099e464c54abb
Yoon jae Park <pyoonj0953@gmail.com>
2018-12-11 14:07:48 +0900

    feat: add  Link, Route in RestaurantSearch

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import NameSearch from "./components/NameSearch";
 import RestaurantList from "./components/RestaurantList/RestaurantList";
 import CartList from "./components/CartList/CartList";
 import Payment from "./components/Payment";
-import OrderTable from './components/OrderTable';
+import OrderTable from "./components/OrderTable";
 import PayResult from "./components/PayResult/PayResult";
 import Order from "./components/Order/Order";
+import RestaurantSearchContainer from "./containers/RestaurantSearchContainer";
 
 class App extends Component {
   render() {
@@ -21,11 +22,14 @@ class App extends Component {
         <Switch>
           <Route exact path={"/"} component={MainPage} />
           {/* 라우터는 여기다가 일단 작성하시오 */}
-          {/* <Route path="/addressSearch" component={AddressSearch} /> */}
           <Route path="/login" component={Login} />
           <Route path="/join" component={Join} />
           <Route path="/search" component={NameSearch} />
           <Route path="/profile" component={ProfilePage} />
+          <Route
+            path="/restaurant-search"
+            component={RestaurantSearchContainer}
+          />
           {/* 레스토랑 리스트 */}
           {/* <Route exact path="/restauant" component={Restaurant} />  */}
           <Route path="/restaurant/:id" component={RestaurantDetailPage} />
