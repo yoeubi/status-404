3b3ca66fb4c0c8c31dd94234e6a1b3a2356059e5
Yoon jae Park <pyoonj0953@gmail.com>
2018-12-12 15:54:53 +0900

    chore: add annotation to RestaurantSearchContainer

--- src/containers/RestaurantSearchContainer.js
+++ src/containers/RestaurantSearchContainer.js
@@ -22,20 +22,22 @@ export default class RestaurantSearchContainer extends Component {
     }
     console.log(searchList);
   }
-
+  // 사용자의 맛집 검색 키워드를 상태로 저장하는 함수
   handleUserInput = e => {
     const userInput = e.target.value;
     this.setState({
       userInput
     });
   };
-
+  // 입력된 맛집 검색 키워드를 다루는 함수
   handleSubmitBtn = () => {
     const { userInput } = this.state;
+    // 상태에 있는 검색 키워드 목록에 사용자 입력을 저장
     this.state.searchList.push(userInput);
+    // local storage에 키워드를 저장
     localStorage.setItem("searchList", JSON.stringify(this.state.searchList));
   };
-
+  // 삭제 버튼으로 검색 키워드 history 목록에 있는 item을 지우는 함수
   handleDeleteBtn = index => {
     this.state.searchList.splice(index, 1);
     localStorage.removeItem("searchList");
