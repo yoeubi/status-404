9f2b918f810be781a0fb82ba0a19c340e4d08e3c
JungwonSeong <yoeubi28@gmail.com>
2018-12-12 11:35:33 +0900

    feat : code splitting

---
+++ src/Pages/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NewLogin from '../components/NewLogin/NewLogin';
+
+const LoginPage = (props) => {
+    return (
+        <NewLogin {...props}/>
+    );
+};
+
+export default LoginPage;
